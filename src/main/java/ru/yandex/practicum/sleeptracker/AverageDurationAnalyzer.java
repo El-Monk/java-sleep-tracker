d3fc93057663ba17cb9de06341d6dab3da832bf6
Monk <drb-monk@yandex.ru>
2026-09-07 23:40:58 +0300

    spaces have been inserted

--- src/main/java/ru/yandex/practicum/sleeptracker/AverageDurationAnalyzer.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/AverageDurationAnalyzer.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class AverageDurationAnalyzer implements SleepAnalyzer {
     @Override
-    public SleepAnalysisResult analyze (List<SleepSession> sessions) {
+    public SleepAnalysisResult analyze(List<SleepSession> sessions) {
         double average = sessions.stream()
                 .map(x -> Duration.between(x.start, x.end).toMinutes())
                 .mapToDouble(x -> x)
